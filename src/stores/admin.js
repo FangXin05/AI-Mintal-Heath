975906afe2400b52b326991ad02d997b44349e42
FangXin <fangxin246@gmail.com>
2026-05-20 10:06:29 +0800

    对pinia仓库添加一些注释说明

--- src/stores/admin.js
+++ src/stores/admin.js
@@ -2,13 +2,13 @@ import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
 export const useAdminStore = defineStore('admin', () => {
+  //定义响应式状态
   const isCollapse = ref(false)
-
-  //   修改状态的方法
+  //定义修改状态的方法
   const toggleCollapse = () => {
     isCollapse.value = !isCollapse.value
   }
-
+  //返回暴露给外部的状态和方法
   return {
     isCollapse,
     toggleCollapse
